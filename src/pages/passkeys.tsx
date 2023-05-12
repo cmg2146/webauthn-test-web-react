9361a1f81fa985d36031780cb157eb813c69c5e6
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2023-05-12 10:32:52 -0700

    add antoehr todo

--- src/pages/passkeys.tsx
+++ src/pages/passkeys.tsx
@@ -3,6 +3,7 @@ import NavSidebar from "@/components/NavSidebar";
 import UserPasskeys from "@/components/UserPasskeys";
 
 export default function Passkeys() {
+  // TODO: Add a layout page to reuse the nav sidebar
   return (
     <div className="relative min-h-screen flex flex-row">
       <NavSidebar></NavSidebar>
