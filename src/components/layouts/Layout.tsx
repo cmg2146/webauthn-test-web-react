fc8f1f75780a155523059ce011b2b922534b983f
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2023-05-16 20:50:44 -0700

    add some useful comments

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({
   headerTitle: string
 }) {
   // TODO: menuVisible should be set false any time screen is lg or larger to ensure
-  // the menu auto hides every time the screen is sized down below the lg breakpoint. The menu is always
+  // the menu auto hides every time the screen is sized down below the lg breakpoint. The sidebar menu is always
   // visible above the lg breakpoint.
   const [menuVisible, setMenuVisible] = useState(false);
 
@@ -22,6 +22,12 @@ export default function Layout({
         className={`w-[300px] shrink-0 h-screen max-lg:fixed z-10 ${menuVisible ? 'left-0' : 'max-lg:-left-[300px]'} overflow-y-auto transition-all duration-300`}
       ></NavSidebar>
       <div className="flex flex-col flex-grow overflow-y-auto">
+        {
+          /*
+            NOTE: The lg breakpoint to show the menu button (to toggle the nav sidebar) is nested
+            in one of the NavHeader elements. TODO: Rework for clarity.
+          */
+        }
         <NavHeader
           title={headerTitle}
           onShowMenuClick={() => setMenuVisible(true)}
