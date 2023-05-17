ce0fff1aa772413eee527ef93c52c28da9a00a57
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2023-05-16 21:29:47 -0700

    next config updates for static export. still not working perfectly.

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ const nextConfig = {
   output: 'export',
   distDir: '.next',
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId },
   ) {
     return {
+      ...defaultPathMap,
       '/': { page: '/passkeys' }
     };
   },
