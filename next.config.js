e4b604f79ca3e024f31143804f7e7a9bab8720b7
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2023-05-17 11:21:57 -0700

    a few fixes for production

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: '.next',
   reactStrictMode: true,
+  // needed for static file routing to work properly for export. Will only work with port 80 and 443 (production only).
+  trailingSlash: true,
   images: {
+    // needed to get export working
     unoptimized: true
   },
   exportPathMap: async function (
--- src/components/layouts/LoginLayout.tsx
+++ src/components/layouts/LoginLayout.tsx
@@ -8,7 +8,7 @@ export default function LoginLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start px-5 py-5 md:px-10">
-      <Image src="webauthn-logo.svg" width="90" height="90" alt="Logo" className="my-10"></Image>
+      <Image src="/webauthn-logo.svg" width="90" height="90" alt="Logo" className="my-10"></Image>
       {children}
     </div>
   );
